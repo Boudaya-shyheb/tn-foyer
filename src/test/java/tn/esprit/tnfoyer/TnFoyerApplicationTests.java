e4d23edee19d82cbf08351bd65c171427f81be76
Shyheb Boudaya <158149244+Shyheb-Boudaya@users.noreply.github.com>
2025-12-14 01:59:07 +0100

    1.6.0

---
+++ src/test/java/tn/esprit/tnfoyer/TnFoyerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.tnfoyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnFoyerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Ce test passe si le contexte Spring démarre sans erreur
+    }
+}
